f1833b44e589d595815c81c95a6a0981e61bc349
daecheol.choi <daecheol.choi@epapyrus.com>
2023-03-22 14:51:37 +0900

    refact: 불필요 클래스 제거

--- core/src/main/java/org/sbs/blog/search/SearchConfiguration.java
+++ core/src/main/java/org/sbs/blog/search/SearchConfiguration.java
@@ -2,18 +2,15 @@ package org.sbs.blog.search;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import org.sbs.blog.YamlPropertyLoader;
 import org.sbs.blog.search.kakako.KakaoApis;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 @Configuration
-@PropertySource(value = "classpath:application.yml", factory = YamlPropertyLoader.class)
 public class SearchConfiguration {
 
 	Logger logger = LoggerFactory.getLogger(OkHttpClient.class);
